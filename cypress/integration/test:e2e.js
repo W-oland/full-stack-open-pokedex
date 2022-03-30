43df8e7b43ac804c56aa1aa97b624d91b0f6f8ca
W-oland <tatu.jarvenranta@gmail.com>
2022-03-30 10:39:56 +0300

    tehtava

--- cypress/integration/test:e2e.js
+++ cypress/integration/test:e2e.js
@@ -11,4 +11,10 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be navigated to', function(){
+    cy.contains('ivysaur').click()
+    cy.contains('speed')
+  })
+
 })
