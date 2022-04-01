55d3aaee1ddb679a3c60e97950337c1b1da4227a
W-oland <tatu.jarvenranta@gmail.com>
2022-04-01 07:09:05 +0300

    tehtava

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
+
+// A simple endpoint for application health check to the backend (11.11)
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('A') // change this to ensure a new version is deployed
+})
